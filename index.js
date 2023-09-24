e9ea98a33502e52b6aa8ad621f0726869ce3c7f1
YuMZyX <joni1koskinen@gmail.com>
2023-09-24 21:14:39 +0300

    Lisätty tehtävät 3.12 - 3-13

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 morgan.token('body', getBody = (request) => {
     return JSON.stringify(request.body)
@@ -15,35 +16,16 @@ app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
 ]
-
-
+/*
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook backend testing</h1>')
-})
+})*/
   
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
