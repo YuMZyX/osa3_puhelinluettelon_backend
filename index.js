f6856c3191a2f38dd13ef6c398a07e93072ab7af
YuMZyX <joni1koskinen@gmail.com>
2023-09-25 17:36:32 +0300

    Lisätty tehtävä 3.22

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-morgan.token('body', getBody = (request) => {
-    return JSON.stringify(request.body)
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    } else if (error.name === 'ValidatorError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'ValidatorError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 
 const app = express()
@@ -28,77 +28,79 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.static('dist'))
-  
+
 app.get('/api/persons', (request, response, next) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
     .catch(error => next(error))
 })
 
 app.get('/info', (request, response, next) => {
-    const date = new Date()
-    Person.countDocuments({}).then(count => {
-        response.send(`Phonebook has info for ${count} people <br /><br />
-        ${date}`)      
-    })
+  const date = new Date()
+  Person.countDocuments({}).then(count => {
+    response.send(`Phonebook has info for ${count} people <br /><br />
+        ${date}`)
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        if (person) {    
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    Person.findByIdAndUpdate(
-        request.params.id, 
-        { name, number }, 
-        { new: true, runValitors: true, context: 'query' })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValitors: true, context: 'query' })
     .then(updatedPerson => {
-        response.json(updatedPerson)
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
 
+/*
 const generateId = () => {
-    const pid = Math.floor(Math.random() * (Math.floor(1000) - Math.ceil(1) + 1) + Math.ceil(1))
-    return pid
+  const pid = Math.floor(Math.random() * (Math.floor(1000) - Math.ceil(1) + 1) + Math.ceil(1))
+  return pid
 }
+*/
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
 
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -15,22 +15,22 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: { 
-      type: String,
-      minlength: 3,
-      required: true
-    },
-    number: {
-      type: String,
-      minlength: 8,
-      required: true,
-      validate: {
-        validator: (v) => {
-          return /^\d{2,3}[-]\d{6,8}$/.test(v)
-        },
-        message: "Incorrect format for phone number"
-      }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}[-]\d{6,8}$/.test(v)
+      },
+      message: 'Incorrect format for phone number'
     }
+  }
 })
 
 personSchema.set('toJSON', {
