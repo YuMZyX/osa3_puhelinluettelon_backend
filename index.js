659ea7599b9fe79152ab1acbb512eea09533bb21
YuMZyX <joni1koskinen@gmail.com>
2023-09-25 16:23:14 +0300

    Lisätty tehtävät 3.19 - 3.21

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    } else if (error.name === 'ValidatorError') {
+        return response.status(400).json({ error: error.message })
     }
   
     next(error)
@@ -54,12 +58,6 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
-  
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
-    }
 
     const person = new Person({
         name: body.name,
@@ -81,14 +79,12 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id, 
+        { name, number }, 
+        { new: true, runValitors: true, context: 'query' })
     .then(updatedPerson => {
         response.json(updatedPerson)
     })
--- models/person.js
+++ models/person.js
@@ -15,8 +15,22 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: { 
+      type: String,
+      minlength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      minlength: 8,
+      required: true,
+      validate: {
+        validator: (v) => {
+          return /^\d{2,3}[-]\d{6,8}/.test(v)
+        },
+        message: "Incorrect format for phone number"
+      }
+    }
 })
 
 personSchema.set('toJSON', {
